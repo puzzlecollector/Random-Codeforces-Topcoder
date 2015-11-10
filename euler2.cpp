048799e5440856b94af534938e812b3014fb8fbb
iljimae0418 <minsukkim9@gmail.com>
2015-11-10 11:25:16 +0900

    Create euler2.cpp

---
+++ euler2.cpp
@@ -0,0 +1,23 @@
+// problem: https://projecteuler.net/problem=2
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+using namespace std; 
+
+// from prior calculations, realised that the 33rd fibonacci number 
+// exceeds four million.  
+typedef unsigned long long ull;
+ull fib[1000] = {1,2}; 
+ 
+int main(){
+	ull sum = 0; 
+	for (int i = 2; i < 32; i++){
+		fib[i] = fib[i-1] + fib[i-2];
+	}
+	for (int i = 0; i < 32; i++){
+		if (fib[i]%2 == 0){
+			sum += fib[i]; 
+		}
+	}
+	cout << sum << endl;
+}
