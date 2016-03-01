69b4483e0020e1914d6c1eba316eeac8ac3f97f2
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2016-02-29 20:34:20 -0800

    Create poj1001.java

---
+++ poj1001.java
@@ -0,0 +1,19 @@
+import java.util.*;  
+import java.math.*;  
+  
+public class Main {  
+    public static void main(String args[]) {  
+        Scanner cin = new Scanner(System.in);  
+        BigDecimal R;  
+        int n;  
+        String s;  
+       
+        R = cin.nextBigDecimal();  
+        n = cin.nextInt();  
+        R = R.pow(n);  
+        s = R.stripTrailingZeros().toPlainString();
+              
+        //if(s.startsWith("0")) s = s.substring(1);
+        System.out.println(s);  
+    }  
+}  
