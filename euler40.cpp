835086a8945e953c15215d6d191aef3ee4fce61d
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-05-21 12:57:18 -0700

    Update euler40.cpp

--- euler40.cpp
+++ euler40.cpp
@@ -15,8 +15,8 @@ int main(){
 		i++; 
 	}
 	int ans = 1; 
-	for (int i = 1; i <= 1000000; i *= 10){
-		ans *= (s[i-1]-'0'); 
+	for (int j = 1; j <= 1000000; j *= 10){
+		ans *= (s[j-1]-'0'); 
 	} 
 	cout << ans << endl; 
 	return 0; 
